860e8468f72c7a3f2da38c0305c3ff80b4f5db0c
Jordan Bent <jordanbent@gmail.com>
2021-06-21 15:31:16 +0100

    c

--- code/MergeService/Startup.cs
+++ code/MergeService/Startup.cs
@@ -18,21 +18,11 @@ namespace MergeService
     [ExcludeFromCodeCoverage]
     public class Startup
     {
-        
-        public Startup(IWebHostEnvironment env)
-        {
-            var builder = new ConfigurationBuilder()
-                .SetBasePath(env.ContentRootPath)
-                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
-                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true);
-            builder.AddEnvironmentVariables();
-            Configuration = builder.Build();
-        }
-        /*
+
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
-        }*/
+        }
 
         public IConfiguration Configuration { get; }
 
@@ -41,7 +31,6 @@ namespace MergeService
         {
 
             services.AddControllers();
-            services.Configure<AppSettings>(options => Configuration.GetSection("AppSettings").Bind(options));
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "MergeService", Version = "v1" });
